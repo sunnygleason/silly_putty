848c6925bec0b5e4c6da58cb0769390a209e269f
Sunny Gleason <sunny.gleason@gmail.com>
2011-10-17 18:15:42 -0400

    symbol -> string for request method

--- lib/silly_putty/clients/net_http.rb
+++ lib/silly_putty/clients/net_http.rb
@@ -5,7 +5,7 @@ module SillyPutty
     private
     def perform_request(method, uri, body, headers)
       Net::HTTP.start(@host, @port) do |http|
-        response = http.send_request(method, uri, body, headers)
+        response = http.send_request(method.to_s, uri, body, headers)
 
         the_headers = {}
         response.each_capitalized {|k,v| the_headers[k] = v}
